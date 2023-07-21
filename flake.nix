61cd96b6bb1b20a100921e10506efa34aeb5df27
waelwindows <waelwindows9922@gmail.com>
2023-07-21 18:38:49 +0300

    chore: Set alejandra as default formatter

--- flake.nix
+++ flake.nix
@@ -77,6 +77,7 @@
         // {
           default = packages.sf-mono;
         };
+      formatter = pkgs.alejandra;
     })
     // {
       overlays.default = final: prev: prev.lib.mapAttrs (mkFont final) sources;
